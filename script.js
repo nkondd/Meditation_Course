1d944770489f01f6c165f1c40bf168c486d55246
nkondd <nkond22@gmail.com>
2023-04-05 18:17:50 +0300

    Add Children Page

--- script.js
+++ script.js
@@ -54,24 +54,33 @@ spaceBetween: 20,
 }
 });
 
-//Select the <a> element
-const itemButton = document.querySelector('.item__button');
-
-//Hide the element on page load
-itemButton.style.opacity = '0';
-
-//Listen for the scroll event on window
-window.addEventListener('scroll', () => {
-  //Check if window's scrollY is bigger than 100vh
-  if(window.scrollY > 400) {
-    //Show the <a> element
-    itemButton.style.opacity = '1';
-    itemButton.style.transition = 'opacity 0.25s ease-in';
-  } else {
-    //Hide the <a> element
-    itemButton.style.opacity = '0';
-    itemButton.style.transition = 'opacity 0.25s ease-out';
-  }
+const swiper3 = new Swiper('.swiper3', {
+  zoom: true,
+  autoplay: {
+  delay: 5000,
+  disableOnInteraction: false,
+},
+pagination: {
+  el: ".swiper-pagination2",
+  clickable: true,
+},
+navigation: {
+nextEl: '.swiper-button-next2',
+prevEl: '.swiper-button-prev2',
+},
+slidesPerView: 1,
+spaceBetween: 10,
+// using "ratio" endpoints
+breakpoints: {
+910: {
+slidesPerView: 2,
+spaceBetween: 20,
+},
+1200: {
+slidesPerView: 3,
+spaceBetween: 20,
+},
+}
 });
 
 const body = document.querySelector("body"),
@@ -90,6 +99,3 @@ body.addEventListener("click" , e =>{
         nav.classList.remove("active");
     }
 });
-
-// Slider
-
